9c28cb4d9f175a593ad46b3d9d98a5b3a9866335
leandrosflora <leandrosflora@gmail.com>
2016-11-22 18:38:45 -0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá!");
+
+            Console.WriteLine("___________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("Aplicação de Exemplo MSDN - Ler Arquivo CSV");
+            Console.WriteLine();
+            Console.WriteLine("___________________________________________");
+            try
+            {
+                //Declaro o StreamReader para o caminho onde se encontra o arquivo 
+                StreamReader rd = new StreamReader(@"C:\Users\flora\Desktop\asd.csv");
+                //Declaro uma string que será utilizada para receber a linha completa do arquivo 
+                string linha = null;
+                //Declaro um array do tipo string que será utilizado para adicionar o conteudo da linha separado 
+                string[] linhaseparada = null;
+                //realizo o while para ler o conteudo da linha 
+                while ((linha = rd.ReadLine()) != null)
+                {
+                    //com o split adiciono a string 'quebrada' dentro do array 
+                    linhaseparada = linha.Split(';');
+                    //aqui incluo o método necessário para continuar o trabalho 
+                    Console.WriteLine(linhaseparada.First().ToString());
+
+                }
+                rd.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Erro ao executar Leitura do Arquivo");
+            }
+
+
+            string connectionString = @"Data Source=convenio.database.windows.net;Initial Catalog=Convenio;Integrated Security=False;User ID=convenio;Password=fl0r@143;Connect Timeout=15;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            SqlConnection conexao = new SqlConnection(connectionString); /* conexao irá conectar o C# ao banco de dados */
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [dbo].[Table]", conexao); /*cmd possui mais de um parâmetro, neste caso coloquei o comando SQL "SELECT * FROM tabela" que irá selecionar tudo(*) de tabela, o segundo parâmetro indica onde o banco está conectado,ou seja se estamos selecionando informações do banco precisamos dizer onde ele está localizado */
+            //Tenta executar o que estiver abaixo
+            try
+            {
+                conexao.Open(); // abre a conexão com o banco   
+                cmd.ExecuteNonQuery();  
+                SqlDataAdapter da = new SqlDataAdapter(); /* Aadapta o banco de dados ao nosso projeto */
+                DataSet ds = new DataSet();
+                da.SelectCommand = cmd; // adapta cmd ao projeto
+                da.Fill(ds); // preenche todas as informações dentro do DataSet                                          
+                //dataGridView1.DataSource = ds; //Datagridview recebe ds já preenchido
+                //dataGridView1.DataMember = ds.Tables[0].TableName;
+                Console.WriteLine(ds.Tables[0].TableName);
+                Console.WriteLine(ds.Tables[0].Columns[0].ColumnName);
+                Console.WriteLine(ds.Tables[0].Columns[1].ColumnName);
+
+                foreach (DataTable table in ds.Tables)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        foreach (DataColumn column in table.Columns)
+                        {
+                            Console.WriteLine(row[column]);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex + "erro");
+                Console.ReadKey();
+            }
+            finally
+            {
+                Console.WriteLine("Fechando conexão");
+                conexao.Close();
+                Console.WriteLine("Conexão fechada");
+            }
+
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ConsoleApplication1/ProgramLeituraCSV.cs
@@ -0,0 +1,142 @@
+﻿//using System; 
+//using System.Data.SqlClient;
+//using System.Linq; 
+//using System.IO;
+
+//namespace ConsoleApplication1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("___________________________________________");
+//            Console.WriteLine("Aplicação de Exemplo - Ler Arquivo CSV e importando no banco de dados");
+//            Console.WriteLine("___________________________________________");
+
+//            string connectionString = @"Data Source=FLORA-PC;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+//            SqlConnection conexao = new SqlConnection(connectionString);  
+//            conexao.Open();  
+//            var transaction = conexao.BeginTransaction();
+//            try
+//            {
+//                StreamReader rd = new StreamReader(@"C:\Users\flora\Desktop\asd.csv");
+//                string linha = null;
+//                string[] linhaseparada = null;
+//                string linhaConcatenada = null;
+//                while ((linha = rd.ReadLine()) != null)
+//                {
+//                    //linhaConcatenada = linha.Replace(';', ',');
+//                    linhaseparada = linha.Split(';');
+//                    int i = 0;
+//                    foreach (var item in linhaseparada)
+//                    {
+//                        if (i == 0)
+//                            linhaConcatenada = "'" + item + "'" + ",";
+//                        else if (i < linhaseparada.Length - 1)
+//                            linhaConcatenada = linhaConcatenada + "'" + item + "'" + ",";
+//                        else
+//                            linhaConcatenada = linhaConcatenada + "'" + item + "'";
+//                        i++;
+//                    }
+//                    var sql = "INSERT INTO [clinica].[dbo].[convenios] VALUES(" + linhaConcatenada + ")";
+//                    SqlCommand cmd = new SqlCommand(sql, conexao, transaction); 
+//                    cmd.ExecuteNonQuery();
+//                    Console.WriteLine(linhaseparada.First().ToString());
+//                }
+//                transaction.Commit();
+//                rd.Close();
+//            }
+//            catch (Exception e)
+//            {
+//                transaction.Rollback();
+//                Console.WriteLine("Erro ao executar Leitura do Arquivo" + e);
+//            }
+//            finally
+//            {
+//                Console.WriteLine("Fechando conexão");
+//                conexao.Close();
+//                Console.WriteLine("Conexão fechada");
+//            }  
+
+//            Console.WriteLine("Press any key to exit.");
+//            Console.ReadKey(); 
+//        }
+//    }
+//}
+
+
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___________________________________________");
+            Console.WriteLine("Aplicação de Exemplo - Ler tabela e manipula no banco de dados");
+            Console.WriteLine("___________________________________________");
+
+            string connectionString = @"Data Source=FLORA-PC;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection conexao = new SqlConnection(connectionString);
+            conexao.Open();
+            var transaction = conexao.BeginTransaction();
+            try
+            {
+                var sqlConsulta = "SELECT id, DataPublicacao FROM [clinica].[dbo].[convenios]";
+                SqlCommand cmdConsulta = new SqlCommand(sqlConsulta, conexao, transaction);
+                cmdConsulta.ExecuteNonQuery();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                da.SelectCommand = cmdConsulta;
+                da.Fill(ds);
+
+                foreach (DataTable table in ds.Tables)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        foreach (DataColumn column in table.Columns)
+                        {
+                            if (column.ColumnName.Equals("DataPublicacao"))
+                            {
+                                Console.WriteLine("Id: " + row[table.Columns[0]] + "---datapublicacao: " + row[column]);
+                                string dataStringDoBanco =  row[column].ToString();
+                                var dataString = dataStringDoBanco.Replace("/", "");
+                                var datea = DateTime.ParseExact(s: dataString, format: "ddMMyyyy",provider: CultureInfo.GetCultureInfo("tr-TR"));
+                                var sqlUpdate = "UPDATE [clinica].[dbo].[convenios] SET DataPublicaoTeste = '" + datea + "' WHERE id=" + row[table.Columns[0]];
+                                SqlCommand cmdUpdate = new SqlCommand(sqlUpdate, conexao, transaction);
+                                cmdUpdate.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception e)
+            {
+                transaction.Rollback();
+                Console.WriteLine("Erro ao executar Leitura do Arquivo" + e);
+            }
+            finally
+            {
+                Console.WriteLine("Fechando conexão");
+                conexao.Close();
+                Console.WriteLine("Conexão fechada");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+
+       
+
+
+    } 
+}
+
+
